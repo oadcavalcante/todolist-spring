7d17d1b73942b7be1b5dafe7204b58fc123d2cf9
3S Cavalcante <aderc19@gmail.com>
2024-12-08 10:00:18 -0300

    :sparkles: CPrimeiro Commit e Criação da entidade Todo.

---
+++ src/main/java/com/oadcavalcante/todolist/todolist_spring/TodolistSpringApplication.java
@@ -0,0 +1,13 @@
+package com.oadcavalcante.todolist.todolist_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oadcavalcante/todolist/todolist_spring/TodolistSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oadcavalcante.todolist.todolist_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
